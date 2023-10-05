602712b8508997467d2f28dc80f386e6e63fb0f3
Nick Meinhold <nick@enspyr.co>
2023-10-05 21:45:47 +1100

    Export dependent packages used by perception apps

--- lib/perception.dart
+++ lib/perception.dart
@@ -1,7 +1,10 @@
 library perception;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:abstractions/beliefs.dart';
+export 'package:abstractions/error_correction.dart';
+export 'package:abstractions/framing.dart';
+export 'package:abstractions/identity.dart';
+export 'package:percepts/percepts.dart';
+export 'package:error_correction_in_perception/error_correction_in_perception.dart';
+export 'package:locator_for_perception/locator_for_perception.dart';
+export 'package:framing_in_perception/framing_in_perception.dart';
